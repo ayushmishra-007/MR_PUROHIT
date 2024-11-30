dd4c1114889a83e810414d15c9c1a2bfc434a79f
ayush766786 <jayushkumar211@gmail.com>
2024-11-30 22:13:42 +0530

    changes in main file

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,15 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import {BrowserRouter} from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import AppContextProvider from './Context/AppContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <AppContextProvider>
-        <App />
+
+      <App />
+
     </AppContextProvider>
-  </BrowserRouter>,
+  </BrowserRouter>
 )
