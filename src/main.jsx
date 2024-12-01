5efc42171e43e320cb0cda29bdb193dac3aaf79f
ayush766786 <jayushkumar211@gmail.com>
2024-12-01 11:57:23 +0530

    toast added

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,25 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
-import AppContextProvider from './Context/AppContext.jsx'
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import { BrowserRouter } from 'react-router-dom';
+import AppContextProvider from './Context/AppContext.jsx';
+import { Toaster } from 'react-hot-toast';
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <AppContextProvider>
-
+      {/* Toaster को Globally Include करें */}
+      <Toaster
+        position="top-right"
+        toastOptions={{
+          duration: 3000,
+          style: {
+            background: "#333",
+            color: "#fff",
+          },
+        }}
+      />
       <App />
-
     </AppContextProvider>
   </BrowserRouter>
-)
+);
